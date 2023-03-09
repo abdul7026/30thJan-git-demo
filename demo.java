6c883eed642e5f875d8ade8f968ef6eee8054e4a
abdul7026 <abdul143hakeemg@gmail.com>
2023-03-09 18:20:13 +0000

    hello world added

---
+++ demo.java
@@ -0,0 +1,10 @@
+pulic class demo{
+	public static void main(strig orgs[]){
+		//prit statemet for hell world 
+		system.out.prit("hello world");
+		//print devops changes
+		system.out.pritl("wel come to devops")
+
+
+      	}
+}
